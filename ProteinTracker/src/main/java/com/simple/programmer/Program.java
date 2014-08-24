5c9e390a4e94a93cee941538e0f2902d5b751d05
mbernardino <moisesb_07@hotmail.com>
2014-08-24 19:41:01 -0300

    inclusão do arquivo de mapeamento .xml hibernate

--- ProteinTracker/src/main/java/com/simple/programmer/Program.java
+++ ProteinTracker/src/main/java/com/simple/programmer/Program.java
@@ -5,7 +5,19 @@ import org.hibernate.Session;
 public class Program {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
+		
 		Session session = HibernateUtilities.getSessionFactory().openSession();
+		session.beginTransaction();
+		
+		User user = new User();
+		
+		user.setName("teste");
+		user.setGoal(250);
+		
+		session.save(user);
+		
+		session.getTransaction().commit();
 		session.close();
+		HibernateUtilities.getSessionFactory().close();
 	}
 }
